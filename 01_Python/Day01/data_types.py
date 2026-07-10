0c670e83f03d17a0cf14a6b3137e8fe4581b83b1
Arvind-Bansal <154428220+Arvind-Bansal@users.noreply.github.com>
2026-07-10 23:37:05 +0530

    Complete Python Day 1 exercises

---
+++ 01_Python/Day01/data_types.py
@@ -0,0 +1,9 @@
+name="arvind"
+age=20
+cgpa=8.9
+boy=True
+fruits={1:"mango",2:"banana",3:"apple"}
+sports=["basketball","badminton","table tennis"]
+subject=("maths","physics","chemistry")
+
+print(f"data type of {name} is {type(name)}.data type of {age} is{type(age)}.data type of {cgpa} is {type(cgpa)}.data type of {boy} is {type(boy)}.data type of {fruits} is {type(fruits)}.data type of {sports} is {type(sports)}.data type of {subject} is {type(subject)}.")
---
+++ 01_Python/Day01/variables.py
@@ -0,0 +1,6 @@
+name="arvind"
+age=20
+college="muit"
+branch="cse"
+
+print(f"my name is {name} and my age is {age}. I am studying in {college} in {branch} branch.")
