ff7756358cd9b44fc2ce51108cf8f70e50794f98
Arvind-Bansal <154428220+Arvind-Bansal@users.noreply.github.com>
2026-07-14 23:24:39 +0530

    performed all types of operations in python

---
+++ 01_Python/Day02/operators.py
@@ -0,0 +1,63 @@
+a=20
+b=30
+
+print("Arithmetic Operators")
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulus
+print(a**b) #exponentiation
+print(a//b) #floor division
+
+print("Comparison Operators")
+print(a>b) #greater than
+print(a<b) #less than
+print(a>=b) #greater than or equal to
+print(a<=b) #less than or equal to
+print(a==b) #equal to
+print(a!=b) #not equal to
+
+x= True
+y= False
+
+print("Logical Operators")
+print(x and y) #logical AND
+print(x or y) #logical OR
+print(not x) #logical NOT
+
+print("Assignment Operators")
+
+num=10
+a=num
+a+= 5
+print(a)
+a-=5
+print(a)
+a*=5
+print(a)
+a/=5
+print(a)
+a%=5
+print(a)
+a//=5
+print(a)
+
+
+a1=10
+b1=20
+print(a1+b1) #addition
+print(a1-b1) #subtraction
+print(a1*b1) #multiplication
+print(a1/b1) #division
+print(a1%b1) #modulus
+print(a1**b1) #exponentiation
+print(a1//b1) #floor division
+
+print(a1>b1) #greater than
+print(a1==b1) #equal to
+
+a1+=5
+print(a1)
+b1+=5
+print(b1)
