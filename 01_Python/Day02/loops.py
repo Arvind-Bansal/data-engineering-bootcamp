0fd8ce355e7523b645b672fe212c3d8b26b5aeee
Arvind-Bansal <154428220+Arvind-Bansal@users.noreply.github.com>
2026-07-23 13:36:33 +0530

    Complete Python Day 2 exercises

---
+++ 01_Python/Day02/loops.py
@@ -0,0 +1,40 @@
+# for i in range(1,21):
+#     print(i)
+
+# for a in range(1,51,2):
+#     print(a)    
+
+# for i in range(1,51):
+#     if i%2==0:
+#         print(i)
+
+# for i in range(1,51):
+#     if i%2!=0:
+#         print(i)
+
+# a=int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(a,"x",i,"=",a*i)
+
+# i=10
+# while(i>=1):
+#     print(i)
+#     i-=1
+
+# for i in range(1,11):
+#     if i==7:
+#         break
+    
+#     print(i)
+
+# for i in range(1,21):
+#     if i%3==0:
+#         continue
+#     print(i)
+
+
+# for i in range(5,0,-1):
+#     print("*"*i)
+
+# for j in range(1,6):
+#     print("*"*j)
